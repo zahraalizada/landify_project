7234e3b18ccebb9e93814e6ec5a27114f84bff4b
Zahra Alizada <elizade.zehra.ez@gmail.com>
2021-03-07 14:46:21 +0400

    first commit

--- js/script.js
+++ js/script.js
@@ -1,35 +1,41 @@
 // partners
 $(".partners-slider").owlCarousel({
-    autoplay: true,
-    nav: true,
-    dots: false,
-    margin: 8,
-    loop: true,
-    items: 5,
-    slideSleep: 1000,
-    paginationSpeed: 1000,
-    smartSpeed: 1000,
-    responsive: {
-        0: {
-            items: 2
-        },
-        380: {
-            items: 2
-        },
-        480: {
-            items: 3
-        },
-        768: {
-            items: 4
-        },
-        992: {
-            items: 5
+        autoplay: true,
+        nav: true,
+        dots: false,
+        margin: 8,
+        loop: true,
+        items: 5,
+        slideSleep: 1000,
+        paginationSpeed: 1000,
+        smartSpeed: 1000,
+        responsive: {
+            0: {
+                items: 2
+            },
+            380: {
+                items: 2
+            },
+            480: {
+                items: 3
+            },
+            768: {
+                items: 4
+            },
+            992: {
+                items: 5
+            }
         }
-    }
+
+    })
+    // wow animate
+$(function() {
+    new WOW().init();
 
 })
 
 $(function() {
-    new WOW().init();
-
+    $('.hamburger-menu').click(function() {
+        $('.lhead-nav').toggleClass('lhead-show')
+    })
 })
